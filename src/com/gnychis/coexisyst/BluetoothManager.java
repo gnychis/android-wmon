44b30537e23f25145c3c310054859a66ce4292ef
George Nychis <gnychis@gmail.com>
2011-03-02 22:17:43 +0000

    trying to fix bluetooth

--- src/com/gnychis/coexisyst/BluetoothManager.java
+++ src/com/gnychis/coexisyst/BluetoothManager.java
@@ -1,9 +1,11 @@
 package com.gnychis.coexisyst;
 
+import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 public class BluetoothManager extends BroadcastReceiver {
 	private static final String TAG = "BTManager";
@@ -16,10 +18,26 @@ public class BluetoothManager extends BroadcastReceiver {
 		scans = 0;
 		this.coexisyst = coexisyst;
 		Log.d(TAG, "startup of BT manager successful");
+        String ts = String.format("Results (%d)\n", scans);
+        coexisyst.textStatus.setText(ts);
 	}
 	
 	public void onReceive(Context context, Intent intent) {
-		
+        String action = intent.getAction();
+        // When discovery finds a device
+        Toast.makeText(coexisyst, "got a callback", Toast.LENGTH_LONG).show();
+        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+            // Get the BluetoothDevice object from the Intent
+            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            // Add the name and address to an array adapter to show in a ListView
+            //mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            String str = String.format("%s - %s", device.getName(), device.getAddress());
+            coexisyst.textStatus.append(str);
+            
+    	    String message = String.format("Found Bluetooth device %s with address %s.",
+    	    		device.getName(), device.getAddress());
+    	    Toast.makeText(coexisyst, message, Toast.LENGTH_LONG).show();
+        }
 		
 	}
 }
