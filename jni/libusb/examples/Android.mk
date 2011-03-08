0c7129fba4518abab2f0d4d52c1aaac4aa1100e8
George Nychis <gnychis@gmail.com>
2011-03-08 01:28:09 +0000

    fixing build

--- jni/libusb/examples/Android.mk
+++ jni/libusb/examples/Android.mk
@@ -3,6 +3,6 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= lsusb.c
 LOCAL_MODULE := lsusb
-LOCAL_C_INCLUDES += jni/
+LOCAL_C_INCLUDES += jni/libusb
 LOCAL_SHARED_LIBRARIES := libc libusb
 include $(BUILD_EXECUTABLE)
