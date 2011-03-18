d9c0b3b91a6dd2a6215e8a947314d52726bb16b2
George Nychis <gnychis@gmail.com>
2011-03-18 18:41:03 +0000

    doing debug out from device driver

--- jni/coexisyst/Android.mk
+++ jni/coexisyst/Android.mk
@@ -5,6 +5,7 @@ LOCAL_SRC_FILES:= coexisyst.c coexisyst_helper.c
 LOCAL_MODULE := coexisyst
 LOCAL_C_INCLUDES += jni/libusb-compat/libusb
 LOCAL_SHARED_LIBRARIES := libc libusb libusb-compat
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog 
 include $(BUILD_SHARED_LIBRARY)
 
 
