04f1a34aa5a5be35f9b0cff86ddd400686356f06
George Nychis <gnychis@gmail.com>
2011-03-20 18:19:28 +0000

    successfully building the wispy library

--- application/jni/Android.mk
+++ application/jni/Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH := $(call my-dir)
 subdirs := $(addprefix $(LOCAL_PATH)/,$(addsuffix /Android.mk, \
     libusb \
     libusb-compat \
+    wispy \
     coexisyst \
 ))
 include $(subdirs)
