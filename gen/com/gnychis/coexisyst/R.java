0ce01537bfcf9754f01950ca06cd3a9bba15e1fc
George Nychis <gnychis@gmail.com>
2011-04-04 14:27:06 +0000

    checking in new bin

--- gen/com/gnychis/coexisyst/R.java
+++ gen/com/gnychis/coexisyst/R.java
@@ -9,7 +9,7 @@ package com.gnychis.coexisyst;
 
 public final class R {
     public static final class array {
-        public static final int countries_array=0x7f050000;
+        public static final int countries_array=0x7f060000;
     }
     public static final class attr {
     }
@@ -17,19 +17,30 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int ScrollView01=0x7f060003;
-        public static final int buttonAdd80211=0x7f060000;
-        public static final int buttonManageDevs=0x7f060002;
-        public static final int buttonManageNets=0x7f060001;
-        public static final int textStatus=0x7f060004;
+        public static final int ScrollView01=0x7f070007;
+        public static final int buttonAdb=0x7f070006;
+        public static final int buttonAddDevice=0x7f070001;
+        public static final int buttonAddNetwork=0x7f070000;
+        public static final int buttonManageDevs=0x7f070002;
+        public static final int buttonNetworkDiag=0x7f070003;
+        public static final int buttonScan=0x7f070004;
+        public static final int buttonViewSpectrum=0x7f070005;
+        public static final int text1=0x7f070009;
+        public static final int text2=0x7f07000a;
+        public static final int text3=0x7f07000b;
+        public static final int textStatus=0x7f070008;
     }
     public static final class layout {
         public static final int devices_list_item1=0x7f030000;
         public static final int devlist=0x7f030001;
         public static final int main=0x7f030002;
+        public static final int three_line_list_item=0x7f030003;
+    }
+    public static final class raw {
+        public static final int lsusb=0x7f040000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
     }
 }
