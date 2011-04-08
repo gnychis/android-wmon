94aa899a68d99f97ac67ca35b83dee63c04c66f7
George Nychis <gnychis@gmail.com>
2011-04-08 06:12:36 +0000

    working subsystem class, but reading has not been tested

--- src/com/gnychis/coexisyst/SubSystem.java
+++ src/com/gnychis/coexisyst/SubSystem.java
@@ -29,11 +29,20 @@ public class SubSystem {
 	}
 	
 	public void cmd(String c) {
+		String in_data;
 		try {
 			_out.writeBytes(c);
 		} catch (Exception e) {
 			Log.e("SYSTEM", "exception trying to run command",e);
 		}
+		
+		/*try {
+			while(true) {
+				in_data = _in.readLine();
+			}
+		} catch (Exception e) {
+			Log.e("SYSTEM", "error trying to read after command", e);
+		}*/
 	}
 	
 	public void install_bin(String b, int resource) {
