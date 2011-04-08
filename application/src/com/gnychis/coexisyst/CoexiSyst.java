7984bac0ca826e0e616ac038143c6599535330ca
George Nychis <gnychis@gmail.com>
2011-04-08 14:35:01 +0000

    working, but going to start factoring in the new atheros code

--- application/src/com/gnychis/coexisyst/CoexiSyst.java
+++ application/src/com/gnychis/coexisyst/CoexiSyst.java
@@ -1,12 +1,5 @@
 package com.gnychis.coexisyst;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 
 import android.app.Activity;
