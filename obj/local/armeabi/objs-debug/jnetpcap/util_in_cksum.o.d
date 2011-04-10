dde638e48572de2bf15099428aa9c5e321e623a3
George Nychis <gnychis@gmail.com>
2011-04-10 23:55:30 +0000

    adding in jnetpcap library, still need to add jar on the next commit (separating because of recent issues)

---
+++ obj/local/armeabi/objs-debug/jnetpcap/util_in_cksum.o.d
@@ -0,0 +1,59 @@
+/Users/gnychis/Documents/workspace/CoexiSyst/obj/local/armeabi/objs-debug/jnetpcap/util_in_cksum.o: \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/jnetpcap/util_in_cksum.cpp \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/jnetpcap/util_in_cksum.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/stdio.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/stdint.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/jni.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/jnetpcap/nio_jbuffer.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/jnetpcap/export.h
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/jnetpcap/util_in_cksum.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/stdio.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/jni.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/jnetpcap/nio_jbuffer.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/jnetpcap/export.h:
