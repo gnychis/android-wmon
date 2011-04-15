cede9d537cc27380cc7da98d9bbf2c9b17a71a57
George Nychis <gnychis@gmail.com>
2011-04-15 16:17:39 +0000

    trying to link properly...

--- jni/libwireshark/Android.mk
+++ jni/libwireshark/Android.mk
@@ -1,12 +1,19 @@
 LOCAL_PATH:= $(call my-dir)
+MY_LOCAL_PATH := $(LOCAL_PATH)
+include $(CLEAR_VARS)
 
+LOCAL_PREBUILT_LIBS := libwireshark.a
+
+include $(BUILD_MULTI_PREBUILT)
+
+LOCAL_PATH := $(MY_LOCAL_PATH)
 include $(CLEAR_VARS)
 NDK_MODULE_PATH := $(LOCAL_PATH)
 LOCAL_SRC_FILES:= wireshark_helper.c 
 LOCAL_MODULE := libwireshark_helper
+LOCAL_PREBUILT_LIBS := libwireshark.a
 LOCAL_C_INCLUDES += jni/libusb-compat/libusb jni/wispy jni/libpcap jni/libwireshark
 LOCAL_SHARED_LIBRARIES := libc libusb libusb-compat libwispy libpcap
-LOCAL_STATIC_LIBRARIES := libwireshark_r36615
-LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -L$(LOCAL_PATH) -llog -lwireshark
 include $(BUILD_SHARED_LIBRARY)
-$(call import-module, libwireshark)
+
