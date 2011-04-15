dce964fe87fa07b646bb102fd583169e8dfaeade
George Nychis <gnychis@gmail.com>
2011-04-15 18:04:09 +0000

    need to properly link to glib

--- jni/libwireshark/Android.mk
+++ jni/libwireshark/Android.mk
@@ -12,8 +12,8 @@ NDK_MODULE_PATH := $(LOCAL_PATH)
 LOCAL_SRC_FILES:= wireshark_helper.c 
 LOCAL_MODULE := libwireshark_helper
 LOCAL_PREBUILT_LIBS := libwireshark.a
-LOCAL_C_INCLUDES += jni/libusb-compat/libusb jni/wispy jni/libpcap jni/libwireshark
-LOCAL_SHARED_LIBRARIES := libc libusb libusb-compat libwispy libpcap
+LOCAL_C_INCLUDES += jni/libusb-compat/libusb jni/wispy jni/libpcap jni/libwireshark jni/libglib jni/libglib/glib jni/libglib/android
+LOCAL_SHARED_LIBRARIES := libc libusb libusb-compat libwispy libpcap libglib-2.0
 LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -L$(LOCAL_PATH) -llog -lwireshark
 include $(BUILD_SHARED_LIBRARY)
 
