cd89740f15e8a09c69c4bcdaecc83c967907ab14
George Nychis <gnychis@gmail.com>
2011-04-16 15:49:54 +0000

    not really building just yet

--- jni/libwireshark/Android.mk
+++ jni/libwireshark/Android.mk
@@ -9,11 +9,11 @@ include $(BUILD_MULTI_PREBUILT)
 LOCAL_PATH := $(MY_LOCAL_PATH)
 include $(CLEAR_VARS)
 NDK_MODULE_PATH := $(LOCAL_PATH)
-LOCAL_SRC_FILES:= wireshark_helper.c 
+LOCAL_SRC_FILES:= wireshark_helper.c
 LOCAL_MODULE := libwireshark_helper
 LOCAL_PREBUILT_LIBS := libwireshark.a
 LOCAL_C_INCLUDES += jni/libusb-compat/libusb jni/wispy jni/libpcap jni/libwireshark jni/libglib jni/libglib/glib jni/libglib/android jni/libwireshark/epan jni/libwireshark/epan/dissectors
-LOCAL_SHARED_LIBRARIES := libc libusb libusb-compat libwispy libpcap libglib-2.0
-LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -L$(LOCAL_PATH) -llog -lwireshark
+LOCAL_SHARED_LIBRARIES := libc libusb libusb-compat libwispy libpcap libglib-2.0 libgmodule-2.0
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -L$(LOCAL_PATH) -llog -lwireshark 
 include $(BUILD_SHARED_LIBRARY)
 
