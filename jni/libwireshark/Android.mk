0734c257cca734d6816ce3702cc71299cca0271b
George Nychis <gnychis@gmail.com>
2011-04-18 15:05:51 +0000

    libwireshark helper is building!

--- jni/libwireshark/Android.mk
+++ jni/libwireshark/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 MY_LOCAL_PATH := $(LOCAL_PATH)
 include $(CLEAR_VARS)
 
-LOCAL_PREBUILT_LIBS := libwireshark.a
+LOCAL_PREBUILT_LIBS := wireshark_libs/libwireshark.a wireshark_libs/libwsutil.a
 
 include $(BUILD_MULTI_PREBUILT)
 
@@ -13,7 +13,7 @@ LOCAL_SRC_FILES:= wireshark_helper.c
 LOCAL_MODULE := libwireshark_helper
 LOCAL_PREBUILT_LIBS := libwireshark.a
 LOCAL_C_INCLUDES += jni/libusb-compat/libusb jni/wispy jni/libpcap jni/libwireshark jni/libglib jni/libglib/glib jni/libglib/android jni/libwireshark/epan jni/libwireshark/epan/dissectors
-LOCAL_SHARED_LIBRARIES := libc libusb libusb-compat libwispy libpcap libglib-2.0 libgmodule-2.0
-LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -L$(LOCAL_PATH) -llog -lwireshark 
+LOCAL_SHARED_LIBRARIES := libc libusb libusb-compat libwispy libpcap libglib-2.0 libgmodule-2.0 libnl
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -L$(LOCAL_PATH) -L$(LOCAL_PATH)/wireshark_libs -llog -lwireshark -lgcc -lwsutil -lz
 include $(BUILD_SHARED_LIBRARY)
 
