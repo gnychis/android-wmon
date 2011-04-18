d2ed916c01c542980896c363a3e251ebadc1a0fc
George Nychis <gnychis@gmail.com>
2011-04-18 05:54:25 +0000

    adding in libnl basic...

---
+++ application/jni/libnl/lib/Android.mk
@@ -0,0 +1,27 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:=\
+	addr.c \
+  attr.c \
+	cache.c\
+  cache_mngr.c \
+  cache_mngt.c \
+  data.c \
+  error.c \
+  handlers.c \
+  msg.c \
+  nl.c \
+  object.c\
+  socket.c \
+  utils.c
+
+
+LOCAL_C_INCLUDES += jni/libnl/include
+
+LOCAL_CFLAGS:=-O2 -g
+LOCAL_CFLAGS+=-DHAVE_CONFIG_H -D_U_="__attribute__((unused))" -Dlinux -D__GLIBC__ -D_GNU_SOURCE
+
+LOCAL_MODULE:= libnl
+
+include $(BUILD_SHARED_LIBRARY)
