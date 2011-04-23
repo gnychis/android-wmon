b865be12e3d67b65ae5e4802515270d058bde968
George Nychis <gnychis@gmail.com>
2011-04-23 02:38:09 +0000

    new version of pcapd to print out debug

--- jni/pcapd/pcapd.c
+++ jni/pcapd/pcapd.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <android/log.h>
 #include <sys/types.h>
 #define LOG_TAG "PcapDriver" // text for log tag 
 
@@ -24,19 +25,19 @@ int main (int argc, char *argv[])
 	struct pcap_pkthdr header;
 	const u_char *packet;
 
-	printf("Size of header: %d\n", sizeof(struct pcap_pkthdr));
+	__android_log_print(ANDROID_LOG_INFO, LOG_TAG, "Size of header: %d\n", sizeof(struct pcap_pkthdr));
 
 	if(argc<3)
 		return -1;
 
 	if (pcap_findalldevs(&alldevs, errbuf) == -1) {
-		printf("error finding pcap devices: %s\n", errbuf);
+		__android_log_print(ANDROID_LOG_INFO, LOG_TAG, "error finding pcap devices: %s\n", errbuf);
 		return -1;
 	}
 
-	printf("Network interfaces:\n");
+	__android_log_print(ANDROID_LOG_INFO, LOG_TAG, "Network interfaces:\n");
 	for(d=alldevs;d;d=d->next) {
-		printf("   %s\n", d->name);
+		__android_log_print(ANDROID_LOG_INFO, LOG_TAG, "   %s\n", d->name);
 	}
 
 	handle = pcap_open_live(argv[1], 0, 1, 1000, errbuf);
@@ -75,7 +76,7 @@ int main (int argc, char *argv[])
 		return -1;
 	}
 
-	printf("Accepted connection\n");
+	__android_log_print(ANDROID_LOG_INFO, LOG_TAG, "Accepted connection\n");
 
 	while(1) {
 		int k,wrote,total;
@@ -94,7 +95,7 @@ int main (int argc, char *argv[])
 			}
 			total += wrote;
 		}
-		printf("Packet Size: %d\n", header.len);
+		__android_log_print(ANDROID_LOG_INFO, LOG_TAG, "Got packet size of: %d", header.len);
 
 		// Now write the pcap packet
 //		total = 0;
