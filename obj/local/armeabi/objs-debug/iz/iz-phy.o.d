99cd42a796c8764a805baef5f251807a39d6b92f
George Nychis <gnychis@gmail.com>
2011-05-17 05:14:42 +0000

    untested working on ath9k_htc integration

---
+++ obj/local/armeabi/objs-debug/iz/iz-phy.o.d
@@ -0,0 +1,194 @@
+/Users/gnychis/Documents/workspace/CoexiSyst/obj/local/armeabi/objs-debug/iz/iz-phy.o: \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/iz-phy.c \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/netlink.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/stdio.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/stdint.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/string.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/malloc.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/stdlib.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/alloca.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/strings.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/memory.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/poll.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/poll.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/poll.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/poll.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/socket.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/socket.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/socket.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/sockios.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/sockios.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/uio.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/time.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/time.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/netlink-compat.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/linux/netlink.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/linux/rtnetlink.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/linux/if_link.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/linux/if_addr.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/linux/neighbour.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../include/linux/genetlink.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/linux/netfilter/nfnetlink.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/types.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/handlers.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/netlink-kernel.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/socket.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/genl/genl.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/msg.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/object.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/utils.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/list.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/attr.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/addr.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/data.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/genl/ctrl.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/cache.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/cache-api.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../include/ieee802154.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/net/if_arp.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/linux/if_arp.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/linux/if_ether.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../include/nl802154.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../include/libcommon.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/iz.h
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/netlink.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/stdio.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/string.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/stdlib.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/alloca.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/strings.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/memory.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/poll.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/poll.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/poll.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/poll.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/socket.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/socket.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/socket.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/sockios.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/sockios.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/uio.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/time.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/time.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/netlink-compat.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/linux/netlink.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/linux/rtnetlink.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/linux/if_link.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/linux/if_addr.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/linux/neighbour.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../include/linux/genetlink.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/linux/netfilter/nfnetlink.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/types.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/handlers.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/netlink-kernel.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/socket.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/genl/genl.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/msg.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/object.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/utils.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/list.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/attr.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/addr.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/data.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/genl/ctrl.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/cache.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/netlink/cache-api.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../include/ieee802154.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/net/if_arp.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/linux/if_arp.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../../libnl/include/linux/if_ether.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../include/nl802154.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/../include/libcommon.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/src/iz.h:
---
+++ obj/local/armeabi/objs-debug/lowpan_common/nl_policy.o.d
@@ -0,0 +1,161 @@
+/Users/gnychis/Documents/workspace/CoexiSyst/obj/local/armeabi/objs-debug/lowpan_common/nl_policy.o: \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/nl_policy.c \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/netlink.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/stdio.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/stdint.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/string.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/malloc.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/stdlib.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/alloca.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/strings.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/memory.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/poll.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/poll.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/poll.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/poll.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/socket.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/socket.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/socket.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/sockios.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/sockios.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/uio.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/time.h \
+ /Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/time.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/netlink-compat.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/linux/netlink.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/linux/rtnetlink.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/linux/if_link.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/linux/if_addr.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/linux/neighbour.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../include/linux/genetlink.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/linux/netfilter/nfnetlink.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/types.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/handlers.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/netlink-kernel.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/socket.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/attr.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/object.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/utils.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/list.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/addr.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/data.h \
+ /Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../include/nl802154.h
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/netlink.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/stdio.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/string.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/stdlib.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/alloca.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/strings.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/memory.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/poll.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/poll.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/poll.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/poll.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/socket.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/socket.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/socket.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/asm/sockios.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/sockios.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/uio.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/sys/time.h:
+
+/Users/gnychis/Documents/android-ndk-r5b/platforms/android-9/arch-arm/usr/include/linux/time.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/netlink-compat.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/linux/netlink.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/linux/rtnetlink.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/linux/if_link.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/linux/if_addr.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/linux/neighbour.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../include/linux/genetlink.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/linux/netfilter/nfnetlink.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/types.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/handlers.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/netlink-kernel.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/socket.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/attr.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/object.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/utils.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/list.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/addr.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../../libnl/include/netlink/data.h:
+
+/Users/gnychis/Documents/workspace/CoexiSyst/jni/lowpan/lib/../include/nl802154.h:
