f0cb6d72aad9fe57a1953a45fc1753de9cb7f5b6
George Nychis <gnychis@gmail.com>
2011-05-22 13:31:51 +0000

    working on wifi state machine

--- gen/com/gnychis/coexisyst/R.java
+++ gen/com/gnychis/coexisyst/R.java
@@ -37,10 +37,12 @@ public final class R {
         public static final int three_line_list_item=0x7f030003;
     }
     public static final class raw {
-        public static final int iwconfig=0x7f040000;
-        public static final int lsusb=0x7f040001;
-        public static final int pcapd=0x7f040002;
-        public static final int zd_firmware=0x7f040003;
+        public static final int disabled_protos=0x7f040000;
+        public static final int htc_7010=0x7f040001;
+        public static final int iwconfig=0x7f040002;
+        public static final int lsusb=0x7f040003;
+        public static final int pcapd=0x7f040004;
+        public static final int update=0x7f040005;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
