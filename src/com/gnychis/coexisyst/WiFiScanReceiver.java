3b2a71527c00a27d51c80f09b7a3bd16a3270a34
George Nychis <gnychis@gmail.com>
2011-07-23 05:54:50 +0000

    ready to test parsing of scan results

--- src/com/gnychis/coexisyst/WiFiScanReceiver.java
+++ src/com/gnychis/coexisyst/WiFiScanReceiver.java
@@ -1,8 +1,10 @@
 package com.gnychis.coexisyst;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Hashtable;
+import java.util.Iterator;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -21,7 +23,7 @@ public class WiFiScanReceiver extends BroadcastReceiver {
   private static final String TAG = "WiFiScanReceiver";
   public String nets_str[];
   private Handler _handler;
-  ArrayList<Packet> _last_scan;
+  public ArrayList<WifiAP> _last_scan;
 
   // If the handler is not null, callbacks will be made
   public WiFiScanReceiver(Handler h) {
@@ -35,9 +37,9 @@ public class WiFiScanReceiver extends BroadcastReceiver {
   
   Comparator<Object> comp = new Comparator<Object>() {
 	public int compare(Object arg0, Object arg1) {
-		if(((ScanResult)arg0).level < ((ScanResult)arg1).level)
+		if(((WifiAP)arg0)._rssi < ((WifiAP)arg1)._rssi)
 			return 1;
-		else if( ((ScanResult)arg0).level > ((ScanResult)arg1).level)
+		else if( ((WifiAP)arg0)._rssi > ((WifiAP)arg1)._rssi)
 			return -1;
 		else
 			return 0;
@@ -46,11 +48,36 @@ public class WiFiScanReceiver extends BroadcastReceiver {
 
   @Override
   public void onReceive(Context c, Intent intent) {
-	ScanResult bestSignal = null;  
-    int i=0;
+    ArrayList<WifiAP> parsed_result = new ArrayList<WifiAP>();
+    Hashtable<String,String> aps_in_list = new Hashtable<String,String>();
     ArrayList<Packet> scan_result = (ArrayList<Packet>) intent.getExtras().get("packets");
     
     Log.d(TAG, "Received incoming scan complete message");
+    
+    // Go through each scan result, and get the access point information
+    Iterator<Packet> results = scan_result.iterator();
+    while(results.hasNext()) {
+    	Packet pkt = results.next();
+    	WifiAP ap = new WifiAP();
+    	
+    	// Kind of like caching the important stuff to be readily accessible
+    	ap._frequency = Integer.parseInt(pkt.getField("radiotap.channel.freq"));
+    	ap._mac = pkt.getField("wlan.sa");
+    	ap._ssid = pkt.getField("wlan_mgt.ssid");
+    	ap._rssi = Integer.parseInt(pkt.getField("radiotap.dbm_antsignal"));
+    	ap._beacon = pkt;
+    	
+    	// Keep the AP if we don't already have a record for it (a single scan
+    	// might catch multiple beacons from the AP).
+    	if(!aps_in_list.containsKey(ap._mac)) {
+    		aps_in_list.put(ap._mac, "");  // mark that we've seen it
+    		parsed_result.add(ap);
+    	}
+    }
+    
+    // Save this scan as our most current scan
+    Collections.sort(parsed_result, comp);
+    _last_scan = parsed_result;
     
     if(_handler != null) {
 		// Send a message to stop the spinner if it is running
