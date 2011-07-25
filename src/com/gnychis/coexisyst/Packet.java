263311b5b3decd57340f2c262407bb1bff9ef3b6
George Nychis <gnychis@gmail.com>
2011-07-25 22:42:41 +0000

    currently working out a bug in the getAll() function, and going to refactor the get() function for wireshark

--- src/com/gnychis/coexisyst/Packet.java
+++ src/com/gnychis/coexisyst/Packet.java
@@ -1,6 +1,8 @@
 package com.gnychis.coexisyst;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Hashtable;
 
 // To store raw packet information.  Can also piggyback a dissection pointer.
 public class Packet implements Serializable {
@@ -61,8 +63,57 @@ public class Packet implements Serializable {
 	    }
 	}
 	
+	public Hashtable<String,ArrayList<String>> getAllFields()
+	{
+		dissect();  // will only dissect if needed
+		String last_tag = "";
+		
+		// First dissect the entire packet, getting all fields
+		String fields[] = wiresharkGetAll(_dissection_ptr);
+		
+		// Now, store all of the fields in a hash table, where each element accesses
+		// an array.  This is done since fields can have multiple values
+		Hashtable<String,ArrayList<String>> pkt_fields;
+		pkt_fields = new Hashtable<String,ArrayList<String>>();
+		
+		// Each field is a descriptor and value, split by a ' '
+		for(int i=0; i<fields.length;i++) {
+			String spl[] = fields[i].split(" ", 2); // spl[0]: desc, spl[1]: value
+			
+			// If it is wlan_mgt.tag.interpretation, start to save the interpretations
+			if(spl[0].equals("wlan_mgt.tag.number")) {
+				last_tag = spl[0] + spl[1];
+				continue;
+			}
+			
+			// No need to save these
+			if(spl[0].equals("wlan_mgt.tag.length"))
+				continue;
+			
+			// Append the tag interpretation to the last tag, we reuse code by
+			// switching spl[0] to last_tag.  The value of the interpretation will
+			// therefore be appended to wlan_mgt.tag.numberX
+			if(spl[0].equals("wlan_mgt.tag.interpretation"))
+				spl[0] = last_tag;
+			
+			// Check the hash table for the key, and then append the value in the
+			// list of values associated.
+			if(pkt_fields.containsKey(spl[0])) {
+				ArrayList<String> l = pkt_fields.get(spl[0]);
+				l.add(spl[1]);
+			} else {
+				ArrayList<String> l = new ArrayList<String>();
+				l.add(spl[1]);
+				pkt_fields.put(spl[0], l);
+			}
+		}
+		
+		return pkt_fields;
+	}
+	
 	// TODO: instead, create a class where all of the wireshark functions are static
 	public native int dissectPacket(byte[] header, byte[] data, int encap);
 	public native void dissectCleanup(int dissect_ptr);
 	public native String wiresharkGet(int dissect_ptr, String param);
+	public native String[] wiresharkGetAll(int dissect_ptr);
 }
