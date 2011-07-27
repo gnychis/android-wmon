86b8ac0721d8fa416f772d625a2c41a602678828
George Nychis <gnychis@gmail.com>
2011-07-27 16:29:25 +0000

    new build for more debugging of wiresharkGetMult

--- application/jni/libwireshark/wireshark_helper.c
+++ application/jni/libwireshark/wireshark_helper.c
@@ -659,7 +659,7 @@ wiresharkGetMult(JNIEnv* env, int wfd_ptr, gchar *field)
   // Copy the results in to our jobjectArray
   for(i=0; i<nresults; i++) {
     jstring strt = (*env)->NewStringUTF( env, dissection->fields->field_values[i]->str );
-	  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "wiresharkGetMult(result[%d]: )", dissection->fields->field_values[i]->str);
+	  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, "wiresharkGetMult(result[%d]: %s)", i, dissection->fields->field_values[i]->str);
     (*env)->SetObjectArrayElement(env, results, i, strt);
   }
 
