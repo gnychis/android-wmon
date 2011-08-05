28ab6a0717068651f79fe7b4fffdc49fb1ef77df
George Nychis <gnychis@gmail.com>
2011-08-05 16:43:28 +0000

    successfully synchronized with the zigbee serial device, reading in the proper number of data bytes

--- jni/zigcapd/zigcapd.c
+++ jni/zigcapd/zigcapd.c
@@ -1,8 +1,11 @@
 #include <errno.h>
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <stdint.h>
+#include <limits.h>
 #include "serial.h"
 
 #define CHANGE_CHAN 0x0000
@@ -14,6 +17,39 @@ void init_econotag();
 	
 #define portname "/dev/ttyUSB1"
 int fd;
+
+char block_read1() {
+	int n;
+	char c;
+
+	while(1) {
+		if((n = read(fd, &c, 1))==1)
+			return c;
+	}
+}
+
+uint32_t block_read_uint32() {
+	int i;
+	uint32_t v = 0;
+
+	for(i=0;i<4;i++) {
+		uint32_t t = ((uint32_t)block_read1()) & 0xff;
+		printf("t(%d): 0x%x\n", i, t);
+		v = v | (t << i*CHAR_BIT);
+	}
+
+	printf("v: 0x%x\n", v);
+
+	return v;
+}
+
+void block_read_nbytes(char *buf, int nbytes) {
+	int nread=0;
+	while(nread<nbytes) {
+		int n = read(fd, buf+nread, nbytes-nread);
+		nread += n;
+	}
+}
 
 int main() {
 	int n;
@@ -29,7 +65,12 @@ int main() {
 		if((n = read(fd, &cmd, 1))==1) {
 			
 			if(cmd==RECEIVED_PACKET) {
-
+				uint8_t lqi = (uint8_t)block_read1();
+				uint32_t rxtime = block_read_uint32();
+				uint8_t length = (uint8_t)block_read1();
+				char *buf = malloc(length);
+				block_read_nbytes(buf, length);
+				printf("rxtime: %d, LQI: %d, length: %d\n", rxtime, lqi, length);
 			}
 
 		}
