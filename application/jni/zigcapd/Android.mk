34df7fb8b4c39e03222555806d913c9357765230
George Nychis <gnychis@gmail.com>
2011-08-05 21:51:13 +0000

    successfully building zigcapd

--- application/jni/zigcapd/Android.mk
+++ application/jni/zigcapd/Android.mk
@@ -1,9 +1,9 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES:= zigcapd.c
+LOCAL_SRC_FILES:= zigcapd.c serial.c
 LOCAL_MODULE := zigcapd
-LOCAL_C_INCLUDES +=
-LOCAL_SHARED_LIBRARIES := libc
+LOCAL_C_INCLUDES += jni/libpcap
+LOCAL_SHARED_LIBRARIES := libc libpcap
 LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog 
 include $(BUILD_EXECUTABLE)
