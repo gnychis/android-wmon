99ff68ee1050a0fc26b533eb82b9e48e3480a008
George Nychis <gnychis@gmail.com>
2011-08-08 01:14:07 +0000

    quick checkin of WIP to detect zigbee device and initialize it

---
+++ application/src/com/gnychis/coexisyst/Zigcapd.java
@@ -0,0 +1,35 @@
+package com.gnychis.coexisyst;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.stericson.RootTools.RootTools;
+
+public class Zigcapd extends AsyncTask<Context, Integer, String>
+{
+	Context parent;
+	int _port;
+	
+	public Zigcapd(int p) {
+		_port = p;
+	}
+	
+	@Override
+	protected String doInBackground( Context ... params )
+	{
+		parent = params[0];
+		String launch_s = "/data/data/com.gnychis.coexisyst/files/zigcapd " + Integer.toString(_port) + " &";
+		
+		try {
+			Log.d("Pcapd", "launching instance of zigcapd on port " + Integer.toString(_port));
+			Log.d("Pcapd", "launch command: " + launch_s);
+			RootTools.sendShell(launch_s);
+		} catch(Exception e) {
+			Log.e("Pcapd", "error trying to start zigcapd daemon",e);
+			return "FAIL";
+		}
+		
+		return "OK";
+	}
+}
