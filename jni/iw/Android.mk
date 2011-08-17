edd20815046599162a2e2f783f62b38b0298a059
George Nychis <gnychis@gmail.com>
2011-08-17 18:21:18 +0000

    adding in a build that is untested, need to continue to migrate

---
+++ jni/iw/Android.mk
@@ -0,0 +1,54 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+#LOCAL_ARM_MODE := arm
+
+LOCAL_SRC_FILES:= \
+	iw.c \
+	genl.c \
+	event.c \
+	info.c \
+	phy.c \
+	interface.c \
+	ibss.c \
+	station.c \
+	survey.c \
+	util.c \
+	mesh.c \
+	mpath.c \
+	scan.c \
+	reg.c \
+	version.c \
+	reason.c \
+	status.c \
+	connect.c \
+	link.c \
+	offch.c \
+	ps.c \
+	cqm.c \
+	bitrate.c \
+	wowlan.c \
+	roc.c \
+	sections.c
+
+
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../android \
+	$(LOCAL_PATH)/../include \
+	$(LOCAL_PATH)/../../libnl/include 
+
+LOCAL_CFLAGS +=  -g
+LOCAL_CFLAGS += -fPIC -DPIC
+
+LOCAL_STATIC_LIBRARIES := libnls libnl
+
+ifeq ($(TARGET_BUILD_TYPE),release)
+	LOCAL_CFLAGS += -g
+endif
+
+LOCAL_MODULE:= liblowpan_common
+
+LOCAL_PRELINK_MODULE := false 
+include $(BUILD_STATIC_LIBRARY)
+
