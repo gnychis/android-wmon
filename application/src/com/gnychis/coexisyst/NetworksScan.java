04d60c41135bc5234e37de517248307e1c77bda2
George Nychis <gnychis@gmail.com>
2011-09-05 07:01:29 +0000

    more major changes that work, putting a significant amount of scanning logic where it belongs now, in the NetworksScan class but not sure why the broadcast receivers cant be in there

--- application/src/com/gnychis/coexisyst/NetworksScan.java
+++ application/src/com/gnychis/coexisyst/NetworksScan.java
@@ -2,7 +2,18 @@ package com.gnychis.coexisyst;
 
 import java.util.ArrayList;
 
+import android.app.Activity;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import com.gnychis.coexisyst.CoexiSyst.ThreadMessages;
+
 /*
+ * Ditch thread messages, use the broadcast as the message of the
+ * scan complete, otherwise it's redundant functionality.  Setup
+ * the broadcast receivers in this class.
+ * 
  * bt.enable();
  * unregisterReceiver(rcvr_BTooth);
  * wifi.setWifiEnabled(true);
@@ -18,21 +29,49 @@ bt.startDiscovery();
 // The purpose of this class is to keep track of a scan taking place across
 // all of the protocols.  That way, we can cache results and determine when
 // each of the protocols has been scanned for.
-public class NetworksScan {
+public class NetworksScan extends Activity {
 	
 	// Keep track of classes which we will interface to scan with
 	Wifi _wifi;
 	ZigBee _zigbee;
 	USBMon _usbmon;
 	
+	// Scan receivers for incoming broadcasts (which include results)
+	public WiFiScanReceiver _rcvr_80211;
+	public ZigBeeScanReceiver _rcvr_ZigBee;
+	//BroadcastReceiver _rcvr_BTooth;
+	
 	public ArrayList<ZigBeeNetwork> _zigbee_scan_result;
 	public ArrayList<WifiAP> _wifi_scan_result;
 	
 	public boolean _is_scanning;  // to ensure we don't double-scan
 	
+	// Setup a handler to receive messages from the broadcast receivers
+	// with the scan results.
+	private Handler _coexisyst_handler;
+	public Handler _handler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			if(msg.obj == ThreadMessages.WIFI_SCAN_COMPLETE) {
+				Log.d("NetworksScan", "Wifi scan is now complete");
+				_wifi_scan_result = _rcvr_80211._last_scan;
+				if(isScanComplete())
+					networkScansComplete();
+			}
+			if(msg.obj == ThreadMessages.ZIGBEE_SCAN_COMPLETE) {
+				Log.d("NetworksScan", "ZigBee scan is now complete");
+				_zigbee_scan_result = _rcvr_ZigBee._last_scan;
+				if(isScanComplete())
+					networkScansComplete();
+			}
+		}
+	};
+	
 	// Set the results to null to begin with, so that we can easily check
 	// when a scan of all protocols is complete.
-	NetworksScan(USBMon m, Wifi w, ZigBee z) {
+	NetworksScan(Handler h, USBMon m, Wifi w, ZigBee z) {
+		
+		_coexisyst_handler = h;
 		
 		// Keep a local copy of the classes
 		_usbmon = m;
@@ -42,6 +81,13 @@ public class NetworksScan {
 		_is_scanning = false;		
 		_zigbee_scan_result = null;
 		_wifi_scan_result = null;
+		
+		// Setup the receivers
+		_rcvr_80211 = new WiFiScanReceiver(_handler);
+		_rcvr_ZigBee = new ZigBeeScanReceiver(_handler);
+		//_rcvr_BTooth = new BluetoothManager(this);
+		//registerReceiver(_rcvr_80211, new IntentFilter(Wifi.WIFI_SCAN_RESULT));
+		//registerReceiver(_rcvr_ZigBee, new IntentFilter(ZigBee.ZIGBEE_SCAN_RESULT));
 	}
 	
 	// For other classes to determine if a scan is already going on
@@ -90,6 +136,16 @@ public class NetworksScan {
 			return false;
 		
 		return true;
+	}
+	
+	private void networkScansComplete() {
+		_usbmon.startUSBMon();
+		_is_scanning=false;
+
+		// Send a message to CoexiSyst that the network scans are complete
+		Message msg = new Message();
+		msg.obj = ThreadMessages.NETWORK_SCANS_COMPLETE;
+		_coexisyst_handler.sendMessage(msg);
 	}
 	
 	// Reset the scan
