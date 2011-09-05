de707626650964030e7f1facc554bf1f946a39f6
George Nychis <gnychis@gmail.com>
2011-09-05 01:33:50 +0000

    successful code refactoring to clean up network scanning, next I need to set scans to be sequential

--- application/src/com/gnychis/coexisyst/NetworksScan.java
+++ application/src/com/gnychis/coexisyst/NetworksScan.java
@@ -23,30 +23,38 @@ public class NetworksScan {
 	// Keep track of classes which we will interface to scan with
 	Wifi _wifi;
 	ZigBee _zigbee;
+	USBMon _usbmon;
 	
 	public ArrayList<ZigBeeNetwork> _zigbee_scan_result;
 	public ArrayList<WifiAP> _wifi_scan_result;
 	
-	public boolean _zigbee_connected;
-	public boolean _wifi_connected;
-	
-	public boolean _is_scanning;
+	public boolean _is_scanning;  // to ensure we don't double-scan
 	
 	// Set the results to null to begin with, so that we can easily check
 	// when a scan of all protocols is complete.
-	NetworksScan(Wifi w, ZigBee z) {
+	NetworksScan(USBMon m, Wifi w, ZigBee z) {
 		
 		// Keep a local copy of the classes
+		_usbmon = m;
 		_wifi = w;
 		_zigbee = z;
 		
+		_is_scanning = false;		
 		_zigbee_scan_result = null;
 		_wifi_scan_result = null;
-		_is_scanning = false;
+	}
+	
+	// For other classes to determine if a scan is already going on
+	public boolean isScanning() {
+		return _is_scanning;
 	}
 	
 	public int initiateScan() {
 		int max=0;	
+		
+		// Just a double check
+		if(_is_scanning || (!_zigbee.isConnected() && !_wifi.isConnected()))
+			return -1;
 		
 		if(_wifi.isConnected()) {
 			if(Wifi._native_scan)
@@ -56,26 +64,37 @@ public class NetworksScan {
 			else
 				max += 1;
 		}
+		
 		if(_zigbee.isConnected())
 			max += ZigBee.channels.length;
 		
-		return 1;
+		_is_scanning=true;
+		
+		_usbmon.stopUSBMon();	// We need to stop the USB monitor, otherwise it interferes
+		resetScanResults();		// Clear the scan results for new results
+		
+		// start the scanning process, which happens in another thread
+		if(_wifi.isConnected())
+			_wifi.APScan();
+		if(_zigbee.isConnected())
+			_zigbee.scanStart();
+		
+		return max;
 	}
 	
 	// A method to check if we have results for each of the networks
 	public boolean isScanComplete() {
-		if(_zigbee_scan_result==null && _zigbee_connected)
+		if(_zigbee_scan_result==null && _zigbee.isConnected())
 			return false;
-		if(_wifi_scan_result==null && _wifi_connected)
+		if(_wifi_scan_result==null && _wifi.isConnected())
 			return false;
 		
 		return true;
 	}
 	
 	// Reset the scan
-	public void resetScan() {
+	public void resetScanResults() {
 		_zigbee_scan_result=null;
 		_wifi_scan_result=null;
-		_is_scanning = false;
 	}
 }
