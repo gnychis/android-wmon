11efa78da3eafc6869abedd02dc9b1606781b012
George Nychis <gnychis@gmail.com>
2012-10-09 22:19:16 -0400

    updating the script

--- init_submodules.sh
+++ init_submodules.sh
@@ -9,7 +9,9 @@ cd ../
 
 # Get the master on the core
 cd core
+git pull
 git checkout master
+git pull
 
 # Initialize and update all of the submodules in core/
 git submodule init
@@ -18,19 +20,25 @@ git submodule update
 # Get the relative branch for each submodule
 cd galaxynexus-cm10-kernel
   git checkout jb
+  git pull
   cd ../
 cd linaro-android-toolchain
   git checkout master
+  git pull
   cd ../
 cd sgs2-cm9-kernel
   git checkout ics
+  git pull
   cd ../
 cd sgs2-skyrocket-kernel
   git checkout master
+  git pull
   cd ../
 cd sgs3-cm10-kernel
   git checkout jellybean
+  git pull
   cd ../
 cd usbhost-kernel
-  git checkout 
+  git checkout coexisyst
+  git pull
   cd ../
