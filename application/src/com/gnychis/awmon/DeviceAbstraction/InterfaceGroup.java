c33107e947df0b6073c46dfed9f086374a303145
George Nychis <gnychis@gmail.com>
2012-11-15 21:22:36 -0500

    making InterfaceScanResult parcelable

--- application/src/com/gnychis/awmon/DeviceAbstraction/InterfaceGroup.java
+++ application/src/com/gnychis/awmon/DeviceAbstraction/InterfaceGroup.java
@@ -2,35 +2,48 @@ package com.gnychis.awmon.DeviceAbstraction;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 
-
-public class InterfaceGroup {
+/** This is just a basic class to keep groups of interfaces together, that will
+ * likely be used to create "devices"
+ * @author George Nychis (gnychis)
+ */
+public class InterfaceGroup implements Parcelable {
 
 	private List<Interface> _members;
 	
-	public InterfaceGroup() {
-		_members = new ArrayList<Interface>();
-	}
+	// The constructors
+	public InterfaceGroup() { _members = new ArrayList<Interface>(); }
+	public InterfaceGroup(List<Interface> initMembers) { _members = initMembers; }
 	
-	public InterfaceGroup(List<Interface> initMembers) {
-		_members = initMembers;
-	}
+	// Methods for accessing and setting members
+	public void setMembers(List<Interface> members) { _members = members; }
+	public void addMembers(List<Interface> members) { _members.addAll(members); }
+	public List<Interface> getMembers() { return _members; }
 	
-	public void setMembers(List<Interface> members) {
-		_members = members;
-	}
+	// For checking if this group contains an interface
+	public boolean hasInterface(Interface iface) { return _members.contains(iface); }
 	
-	public void addMembers(List<Interface> members) {
-		_members.addAll(members);
-	}
-	
-	
-	public boolean hasInterface(Interface iface) {
-		return _members.contains(iface);
-	}
-	
-	public List<Interface> getMembers() {
-		return _members;
-	}
+    // ********************************************************************* //
+    // This code is to make this class parcelable and needs to be updated if
+    // any new members are added to the Device class
+    // ********************************************************************* //
+    public int describeContents() {
+        return this.hashCode();
+    }
+    
+    public static final Creator<InterfaceGroup> CREATOR = new Creator<InterfaceGroup>() {
+        public InterfaceGroup createFromParcel(Parcel source) { return new InterfaceGroup(source); }
+        public InterfaceGroup[] newArray(int size) { return new InterfaceGroup[size]; }
+    };
 
+    public void writeToParcel(Parcel dest, int parcelableFlags) {
+    	dest.writeList(_members);
+    }
+    
+    private InterfaceGroup(Parcel source) {
+    	source.readList(_members, this.getClass().getClassLoader());
+    }
+    
 }
